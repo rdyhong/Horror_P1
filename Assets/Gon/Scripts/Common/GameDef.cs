ff0b0a114b02ae1e707a02fd95018782accd4879
gon <rdyhong@gmail.com>
2023-11-15 01:01:44 +0900

    - 디파인 코드 수정

--- Assets/Gon/Scripts/Common/GameDef.cs
+++ Assets/Gon/Scripts/Common/GameDef.cs
@@ -3,12 +3,14 @@ public static class GameDef
     // PlayerPrefs Key
     public static readonly string PF_KEY_MOUSE_SENSITIVE_X = "";
     public static readonly string PF_KEY_MOUSE_SENSITIVE_Y = "";
+
     public static readonly float MOUSE_SENSITIVE_X = 2;
     public static readonly float MOUSE_SENSITIVE_Y = 2;
     // Other
     public static readonly float PLAYER_SPEED = 2;
     public static readonly float PLAYER_SIGHT_RAY_LENGTH = 2f;
 
+    // String Def
     public static readonly string SINGLETON_PARENT_NAME = "Singleton";
     public static readonly string INPUT_AXIS_VERTICAL = "Vertical";
     public static readonly string INPUT_AXIS_HORIZONTAL = "Horizontal";
