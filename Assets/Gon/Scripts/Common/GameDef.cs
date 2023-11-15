bf003aa077e559a96e4a8f60a65d3b7e6f25b9f7
gon <rdyhong@gmail.com>
2023-11-15 23:40:37 +0900

    - 폴더 추가, 리소스 추가

--- Assets/Gon/Scripts/Common/GameDef.cs
+++ Assets/Gon/Scripts/Common/GameDef.cs
@@ -10,7 +10,7 @@ public static class GameDef
     public static readonly float PLAYER_SIGHT_RAY_LENGTH = 2f;
     public static readonly float PLAYER_HEAD_ROTATE_X_MIN = -80;
     public static readonly float PLAYER_HEAD_ROTATE_X_MAX = 80;
-    public static readonly float PLAYER_BASE_SPEED = 2;
+    public static readonly float PLAYER_BASE_SPEED = 3;
 
     // String Def
     public static readonly string SINGLETON_PARENT_NAME = "Singleton";
