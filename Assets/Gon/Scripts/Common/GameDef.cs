ad1f3254c29461008871f257a63db1e802d46938
Hazzys <fhsxm7772@gmail.com>
2023-11-19 00:45:29 +0900

    - 로그 전처리기 추가, 플레이어 속도 수정

--- Assets/Gon/Scripts/Common/GameDef.cs
+++ Assets/Gon/Scripts/Common/GameDef.cs
@@ -3,7 +3,7 @@ public static class GameDef
     public static readonly float PLAYER_SIGHT_RAY_LENGTH = 2f;
     public static readonly float PLAYER_HEAD_ROTATE_X_MIN = -80;
     public static readonly float PLAYER_HEAD_ROTATE_X_MAX = 80;
-    public static readonly float PLAYER_BASE_SPEED = 3;
+    public static readonly float PLAYER_BASE_SPEED = 4;
 
     // String Def
     public static readonly string SINGLETON_PARENT_NAME = "Singleton";
--- Assets/Gon/Scripts/Reusable/DebugUtil.cs
+++ Assets/Gon/Scripts/Reusable/DebugUtil.cs
@@ -4,26 +4,30 @@ public static class DebugUtil
 {
     public static void Log(string log)
     {
-#if UNITY_EDITOR
+#if LOG_ENABLE
         Debug.Log(log);
 #endif
     }
     public static void LogWarn(string log)
     {
-#if UNITY_EDITOR
+#if LOG_ENABLE
         Debug.LogWarning(log);
 #endif
     }
     public static void LogErr(string log)
     {
-#if UNITY_EDITOR
+#if LOG_ENABLE
         Debug.LogError(log);
 #endif
     }
     public static void LogAssert(bool condition, string log)
     {
-#if UNITY_EDITOR
+#if LOG_ENABLE
         Debug.Assert(condition, log);
+        if (!condition)
+        {
+            throw new System.Exception(log);
+        }
 #endif
     }
 }
