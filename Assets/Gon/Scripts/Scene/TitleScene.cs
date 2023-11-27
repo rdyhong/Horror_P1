3cb51f375594e56e313969c391e1816a14f9213b
Gon <rdyhong@gmail.com>
2023-11-28 04:03:15 +0900

    - 타이틀씬 애니메이션 추가

--- Assets/Gon/Scripts/Scene/TitleScene.cs
+++ Assets/Gon/Scripts/Scene/TitleScene.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class TitleScene : MonoBehaviour
 {
-    [SerializeField] Animator _camAnimator;
+    [SerializeField] Animator _titleAnimator;
 
     void Start()
     {
-        UIMgr.Inst.Push<TitlePanel>();
-
-        _camAnimator.enabled = true;
+        StartCoroutine(TitleAnim());
     }
 
+    IEnumerator TitleAnim()
+    {
+        yield return new WaitForSeconds(1f);
+        _titleAnimator.Play("TitleAnim");
+
+        yield return null;
+        yield return new WaitUntil(() => _titleAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f);
+
+        yield return new WaitForSeconds(1f);
+
+        UIMgr.Inst.Push<TitlePanel>();
+    }
 }
--- Assets/Gon/Scripts/UI/TitlePanel.cs
+++ Assets/Gon/Scripts/UI/TitlePanel.cs
@@ -5,17 +5,34 @@ using UnityEngine.UI;
 
 public class TitlePanel : UIRoot
 {
+    [SerializeField] CanvasGroup _canvasGroup;
     [SerializeField] Button _startButton;
 
     private void Awake()
     {
+        _canvasGroup.alpha = 0;
+        _startButton.interactable = false;
         _startButton.onClick.AddListener(() => {
             OnClickStartButton();
         });
+
+        StartCoroutine(ShowButtonAnimCo());
     }
 
     void OnClickStartButton()
     {
         SceneMgr.Inst.LoadScene(ESceneType.Main);
+    }
+
+    IEnumerator ShowButtonAnimCo()
+    {
+        while (true)
+        {
+            yield return null;
+            _canvasGroup.alpha = _canvasGroup.alpha + 0.1f* Time.deltaTime;
+            if (_canvasGroup.alpha >= 1f) break;
+        }
+
+        _startButton.interactable = true;
     }
 }
