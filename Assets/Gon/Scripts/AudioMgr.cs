c64e4f5b7cb353ac42bb380b3da68f82dbd90fc2
Gon <rdyhong@gmail.com>
2023-12-04 09:59:16 +0900

    - 오디오 매니저 수정

--- Assets/Gon/Scripts/AudioMgr.cs
+++ Assets/Gon/Scripts/AudioMgr.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public enum ESoundTypeFoot
 {
     Grass,
     Wood,
     Rock,
+}
+public enum ESoundType
+{
+    Door_Open,
+    Door_Close,
+
 }
 
 public class AudioMgr : Singleton<AudioMgr>
@@ -34,5 +41,11 @@ public class AudioMgr : Singleton<AudioMgr>
                 eff.PlayEffect(_footRock[0]);
                 break;
         }
+    }
+
+    public void PlayEffect(Vector3 pos)
+    {
+        AudioEffect eff = ResourcesMgr.Inst.Spawn<AudioEffect>(EResourcePath.Audio);
+        eff.transform.position = pos;
     }
 }
