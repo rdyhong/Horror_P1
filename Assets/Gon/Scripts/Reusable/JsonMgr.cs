b6335b73dca674d0e156645a6f2d0ed55bf579e6
Gon <rdyhong@gmail.com>
2023-12-05 09:43:27 +0900

    - Newtonsoft Json SDK 임포트, Json 로드 수정중

--- Assets/Gon/Scripts/Reusable/JsonMgr.cs
+++ Assets/Gon/Scripts/Reusable/JsonMgr.cs
@@ -3,14 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class JsonMgr : Singleton<JsonMgr>
 {
     public Dictionary<int, Data_Item> data_Items = new Dictionary<int, Data_Item>();
-    
+
     public void Init()
     {
         data_Items = LoadData<Data_Item>();
+
+        foreach(int key in data_Items.Keys)
+        {
+            DebugUtil.Log($"{data_Items[key].Name_KR}");
+        }
     }
 
     Dictionary<int, T> LoadData<T>()
@@ -21,11 +27,14 @@ public class JsonMgr : Singleton<JsonMgr>
         // 파일의 텍스트를 string으로 저장
         TextAsset textData = Resources.Load($"JsonData/{name}") as TextAsset;
 
-
+        DebugUtil.Log($"{textData.text}");
         //string jsonData = File.ReadAllText(path);
         // 이 Json데이터를 역직렬화하여 playerData에 넣어줌
+        //T[] val = JsonUtility.FromJson<T[]>(textData.ToString());
+        //JsonConvert.DeserializeObject < Dictionary<int, T>>(textData.ToString());
+        Dictionary<int, T> val = JsonConvert.DeserializeObject<Dictionary<int, T>>(textData.text);
 
-        return JsonUtility.FromJson<Dictionary<int, T>>(textData.ToString());
+        return val;
     }
 
     void SaveData<T>(object data)
@@ -43,9 +52,9 @@ public class JsonMgr : Singleton<JsonMgr>
 [Serializable]
 public class Data_Item
 {
-    string Name_KR = string.Empty;
-    string Name_EN = string.Empty;
-    bool Consumable;
-    int[] ConbinableKey;
-    bool Holdable;
+    public string Name_KR = string.Empty;
+    public string Name_EN = string.Empty;
+    public bool Consumable;
+    public int[] ConbinableKey;
+    public bool Holdable;
 }
